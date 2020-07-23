23ee3571716f74576970ffa88578416aa83c0d27
Will Scofield <wiscofield@davidson.edu>
2020-07-23 14:47:29 -0400

    work on script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const MAX_QUESTIONS = 6;
+
 /*---MATH---*/
+
 //user object
 let user = {
     //Stats
@@ -10,32 +13,80 @@ let user = {
 
 //functions
 function addToStat(points, stat) {
-    user['stat'] += points;
-    console.log();
+    //BIG PROBLEM (SYNTAX)
+    user[stat] += points;
+    console.log(stat + user[stat]);
 }
 
 function setStat(points, stat) {
-    user['stat'] = points;
-    console.log();
+    user[stat] = points;
 }
 
 function getStat(stat) {
-    return user['stat'];
-    console.log();
+    return user[stat];
 }
 
 function highStat() {
     return Object.keys(user).reduce(function(a, b){return user[a] > user[b] ? a : b});
     //Yes I did steal this from the internet but I do understand it!
-    console.log();
 }
 
 /*---QUIZ---*/
 var qNum = 0;
 
+/**
+ * Moves to the next question
+ */
 function nextQuestion() {
-    document.querySelector('.question#q' + qNum).style.display = 'none';
+    if(qNum === 0) {
+        //Change button to next
+        buttonToNext();
+    }
+
+    //BIG PROBLEM (LOGIC)
     qNum++;
-    document.querySelector('.question#q' + qNum).style.display = 'block';
-    console.log();
+    if (qNum < MAX_QUESTIONS + 1 && qNum > 0) {
+        document.querySelector('.question#q' + (qNum - 1)).style.display = 'none';
+        respond();
+        document.querySelector('.question#q' + qNum).style.display = 'block';
+    }
+
+    if(qNum == 6) {
+        //Change button to submit
+        buttonToSubmit();
+    }
+    if(qNum == 7) {
+        document.querySelector('.question#q' + (qNum - 1)).style.display = 'none';
+        hideButton();
+        onSubmit();
+    }
+}
+
+function respond() {
+    let stat = document.querySelector('input[name=q' + qNum + ']').value;
+
+    addToStat(1, stat);
+}
+
+function buttonToNext() {
+    console.log('Button updated to next');
+    let button = document.querySelector('#next');
+    button.innerText = 'Next';
+}
+
+function buttonToSubmit() {
+    console.log('Button updated to submit');
+    let button = document.querySelector('#next');
+    button.innerText = 'Submit';
+}
+
+function hideButton() {
+    console.log('Hiding button');
+    let button = document.querySelector('#next');
+    button.style.display = 'none';
+}
+
+function onSubmit() {
+    //Show highStat() and links
+    console.log(highStat());
 }
---
+++ videogame.js
@@ -0,0 +1,19 @@
+// let gameOne = document.querySelector('.game-one');
+// gameOne.addEventListener('click', function() {
+//   alert('You Clicked Game One');
+// });
+
+// let gameTwo = document.querySelector('.game-two');
+// gameOne.addEventListener('click', function() {
+//   alert('You Clicked Game Two');
+// });
+
+// let gameThree = document.querySelector('.game-three');
+// gameOne.addEventListener('click', function() {
+//   alert('You Clicked Game Three');
+// });
+
+// let gameFour = document.querySelector('.game-four');
+// gameOne.addEventListener('click', function() {
+//   alert('You Clicked Game four');
+// });
